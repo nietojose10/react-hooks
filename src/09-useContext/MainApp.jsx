43974d13a5ff16db25b93460a2ecd001adda51be
Jose <nietojose1995@gmail.com>
2023-04-25 23:35:11 +0100

    Adding changes of useContext lessons

---
+++ src/09-useContext/MainApp.jsx
@@ -0,0 +1,32 @@
+import { Link, Navigate, Route, Routes } from "react-router-dom"
+import { HomePage } from './HomePage';
+import { AboutPage } from './AboutPage';
+import { LoginPage } from './LoginPage';
+import { Navbar } from "./Navbar";
+import { UserProvider } from "./context/UserProvider";
+
+export const MainApp = () => {
+  return (
+    <UserProvider>
+        {/* <h1>MainApp</h1> */}
+        {/* <Link to="/" >Home</Link>
+        <Link to="/about" >About</Link>
+        <Link to="/login" >Login</Link> */}
+        <Navbar/>
+        <hr />
+
+        <Routes>
+            <Route path="/" element={ <HomePage /> } />
+            <Route path="/about" element={ <AboutPage /> } />
+            <Route path="/login" element={ <LoginPage /> } />
+
+            {/*Si colocan una ruta que no existe, este codigo redigira al componente de login */}
+            {/* <Route path="/*" element={ <LoginPage /> } /> */}
+            <Route path="/*" element={ <Navigate to="/about" /> } />
+
+
+        </Routes>
+
+    </UserProvider>
+  )
+}
--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 // import { HooksApp } from './HooksApp';
 import './index.css';
+import { BrowserRouter, createBrowserRouter, RouterProvider } from 'react-router-dom';
 // import { CounterApp } from './01-useState/CounterApp';
 //import { CounterWithCustomHook } from './01-useState/CounterWithCustomHook';
 //import { SimpleForm } from './02-useEffect/SimpleForm';
@@ -14,10 +15,16 @@ import './index.css';
 //import { CallbackHook } from './06-memos/CallbackHook';
 // import { Padre } from './07-tarea-memo/Padre';
 // import './08-useReducer/intro-reducer';
-import { TodoApp } from './08-useReducer/TodoApp';
+//import { TodoApp } from './08-useReducer/TodoApp';
+import { MainApp } from './09-useContext/MainApp';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
-    <TodoApp />
+    <BrowserRouter>
+    
+      <MainApp />
+    
+    </BrowserRouter>
+    
   // </React.StrictMode>,
 )
